caa8b786b05ea4d2214d2bcc64ea02a2dbd1037e
Sparsh Gautam <sparsh@sparshs-MacBook-Air.local>
2026-06-14 14:31:53 +0530

    Replace localhost with production API URL

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -43,7 +43,7 @@ const Signup = () => {
     console.log(formData);
     try {
         setLoading(true);
-        const response = await axios.post(`http://localhost:8000/api/v1/user/register`, formData, {
+        const response = await axios.post(`${import.meta.env.VITE_URL}/user/register`, formData, {
             headers: {
                 "Content-Type":"application/json"
             }

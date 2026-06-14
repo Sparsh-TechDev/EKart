b94a09364197e88b53e9ad81d2ed8d446c0e2127
Sparsh Gautam <sparsh@sparshs-MacBook-Air.local>
2026-06-14 15:41:57 +0530

    Updated nodemailer

--- backend/emailVerify/sendOtpMail.js
+++ backend/emailVerify/sendOtpMail.js
@@ -2,30 +2,27 @@ import nodemailer from "nodemailer";
 import "dotenv/config";
 
 export const sendOtpMail = async (otp, email) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
+  console.log("Sending OTP to:", email);
 
-    const mailConfigurations = {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  try {
+    const info = await transporter.sendMail({
       from: process.env.MAIL_USER,
       to: email,
       subject: "Password Reset OTP",
-      html: `<p>Your OTP for password reset is <b>${otp}</b></p>`,
-    };
-
-    const info = await transporter.sendMail(mailConfigurations);
+      html: `<p>Your OTP is <b>${otp}</b></p>`,
+    });
 
     console.log("OTP Sent Successfully");
     console.log(info.response);
-
-    return true;
   } catch (error) {
-    console.error("Email Error:", error);
-    return false;
+    console.error("MAIL ERROR:", error);
   }
 };

6504c400d728496a7332855bba48c1ad3007e1fb
Sparsh Gautam <sparsh@sparshs-MacBook-Air.local>
2026-06-30 21:21:24 +0530

    Fix OTP

--- backend/emailVerify/sendOtpMail.js
+++ backend/emailVerify/sendOtpMail.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 // Initialize the transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.MAIL_USER,
